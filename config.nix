697f8199fc66ced8f002860ae62ea3884d858f9c
Anwer Khan <deepwatrcreatur@gmail.com>
2025-04-02 07:33:12 -0400

    list of packages installed for local user

---
+++ config.nix
@@ -0,0 +1,35 @@
+{
+packageOverrides = pkgs: {
+    myPackages = pkgs.buildEnv {
+      name = "my-packages";
+      paths = with pkgs; [
+      yt-dlp
+      helix
+      rclone
+      git
+      gh
+      bash
+      python3
+      kitty
+      nano
+      neovim
+      chezmoi
+      stow
+      gnupg
+      fastfetch
+      rclone
+      elixir
+      erlang
+      mix2nix
+      iperf3
+      htop
+      btop
+      fzf
+      bat
+      wget
+      curl
+      tmux
+      ];
+    };
+  };
+}
