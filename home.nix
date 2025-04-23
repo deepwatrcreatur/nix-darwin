a58aa02ff008106645b3d477219e8179bf017fab
Anwer Khan <deepwatrcreatur@gmail.com>
2025-04-23 03:14:08 -0400

    added some configuration and plugins for fish

--- home.nix
+++ home.nix
@@ -5,18 +5,6 @@
   home.homeDirectory = "/Users/deepwatrcreatur";
   home.stateVersion = "24.11";
 
-  # Add packages to your PATH
-  home.packages = with pkgs; [
-    # Development tools
-    # Other utilities you might want
-    rsync
-    ripgrep
-    fd
-    jq
-    fish
-    lsd
-  ];
-
   shellInit = ''
     set -x EDITOR hx
     set -x VISUAL hx
@@ -34,6 +22,51 @@
       lla = "lsd -la";
       ".." = "cd ..";
       update = "darwin-rebuild switch --flake $(home.homeDirectory)/nix-darwin-config/#$(hostname)"; # Example update alias
+    enable = true; # Make sure fish is enabled in home-manager
+    plugins = [
+      { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish; }
+      { name = "z"; src = pkgs.fishPlugins.z; } # Directory jumping
+      { name = "puffer-fish"; src = pkgs.fishPlugins.puffer-fish; } # Text expansions
+      { name = "autopair"; src = pkgs.fishPlugins.autopair; } # Autopair brackets/quotes
+      { name = "grc"; src = pkgs.fishPlugins.grc; } # Colorize command output (needs grc package too)
+      { name = "bobthefish"; src = pkgs.fishPlugins.bobthefish; }
+    ];
+
+    interactiveShellInit = ''
+      # === Bobthefish theme config ===
+      # set -g theme_nerd_fonts yes # Set this if you have Nerd Fonts installed
+      set -g theme_color_scheme solarized
+      set -g theme_powerline_fonts no
+      set -g theme_project_dir_length 1
+      set -g theme_show_exit_status yes
+      set -g theme_display_git_untracked yes
+
+      # === Tide theme config (if using Tide) ===
+      # Run 'tide configure' in your shell once, then copy the settings here
+      # set -g tide_left_prompt_items (...)
+      # set -g tide_right_prompt_items (...)
+      # ... other tide settings ...
+
+      # === Other plugin settings ===
+      # set -g Z_SCRIPT_PATH ${pkgs.zoxide}/bin/zoxide # If using zoxide instead of fishPlugins.z
+    '';
+
+    # Add any packages needed by plugins (like grc, fzf, etc.)
+    # fishPlugins.grc needs the core 'grc' package
+    # fishPlugins.fzf-fish needs 'fzf'
+  };
+
+  # Make sure necessary tools are installed
+  home.packages = [
+    pkgs.fish
+    pkgs.fzf # Needed by fzf-fish plugin
+    pkgs.grc # Needed by grc plugin
+    # pkgs.zoxide # Alternative to fishPlugins.z
+    # pkgs.nerdfonts # If your theme requires it
+    pkgs.lsd # Since you wanted this earlier
+  ];
+
+
     };
   programs.helix = {
     enable = true;
