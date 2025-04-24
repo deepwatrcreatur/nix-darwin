6d32d5d4d53c9783025eadda8c2e9b32afde2e8b
Anwer Khan <deepwatrcreatur@gmail.com>
2025-04-24 01:36:14 -0400

    Forec homeDirectory in home.nix

--- home.nix
+++ home.nix
@@ -1,134 +1,73 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+let
+  username = "deepwatrcreatur";
+  homeDirectory = "/Users/${username}";
 
+in
 {
-  home.username = "deepwatrcreatur";
-  home.homeDirectory = "/Users/deepwatrcreatur";
-  home.stateVersion = "24.11";
-
-  shellInit = ''
-    set -x EDITOR hx
-    set -x VISUAL hx
-  '';
-
-   users.users.${username} = {
-    shell = pkgs.fish;
-  };
+  home.username = username;
+  home.homeDirectory = lib.mkForce homeDirectory;
+  home.stateVersion = "24.11"; # Please update this to your actual state version
 
   programs.fish = {
+    enable = true;
+    shellInit = ''
+      set -gx EDITOR hx
+      set -gx VISUAL hx
+    '';
     shellAliases = {
-      ls = "lsd"; # If you use lsd
+      ls = "lsd";
       ll = "lsd -l";
       la = "lsd -a";
       lla = "lsd -la";
       ".." = "cd ..";
-      update = "darwin-rebuild switch --flake $(home.homeDirectory)/nix-darwin-config/#$(hostname)"; # Example update alias
-    enable = true; # Make sure fish is enabled in home-manager
+      update = "darwin-rebuild switch --flake ${homeDirectory}/nix-darwin-config/#$(hostname)";
+    };
     plugins = [
       { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish; }
-      { name = "z"; src = pkgs.fishPlugins.z; } # Directory jumping
-      { name = "puffer-fish"; src = pkgs.fishPlugins.puffer-fish; } # Text expansions
-      { name = "autopair"; src = pkgs.fishPlugins.autopair; } # Autopair brackets/quotes
-      { name = "grc"; src = pkgs.fishPlugins.grc; } # Colorize command output (needs grc package too)
+      { name = "z"; src = pkgs.fishPlugins.z; }
+      { name = "puffer-fish"; src = pkgs.fishPlugins.puffer-fish; }
+      { name = "autopair"; src = pkgs.fishPlugins.autopair; }
+      { name = "grc"; src = pkgs.fishPlugins.grc; }
       { name = "bobthefish"; src = pkgs.fishPlugins.bobthefish; }
     ];
-
     interactiveShellInit = ''
-      # === Bobthefish theme config ===
-      # set -g theme_nerd_fonts yes # Set this if you have Nerd Fonts installed
       set -g theme_color_scheme solarized
       set -g theme_powerline_fonts no
       set -g theme_project_dir_length 1
       set -g theme_show_exit_status yes
       set -g theme_display_git_untracked yes
-
-      # === Tide theme config (if using Tide) ===
-      # Run 'tide configure' in your shell once, then copy the settings here
-      # set -g tide_left_prompt_items (...)
-      # set -g tide_right_prompt_items (...)
-      # ... other tide settings ...
-
-      # === Other plugin settings ===
-      # set -g Z_SCRIPT_PATH ${pkgs.zoxide}/bin/zoxide # If using zoxide instead of fishPlugins.z
     '';
-
-    # Add any packages needed by plugins (like grc, fzf, etc.)
-    # fishPlugins.grc needs the core 'grc' package
-    # fishPlugins.fzf-fish needs 'fzf'
   };
 
-  # Make sure necessary tools are installed
-  home.packages = [
-    pkgs.fish
-    pkgs.fzf # Needed by fzf-fish plugin
-    pkgs.grc # Needed by grc plugin
-    # pkgs.zoxide # Alternative to fishPlugins.z
-    # pkgs.nerdfonts # If your theme requires it
-    pkgs.lsd # Since you wanted this earlier
-  ];
-
-
-    };
   programs.helix = {
     enable = true;
     defaultEditor = true;
-
     extraPackages = with pkgs; [
-      # Nix language servers
       nil
       nixd
-      nixfmt-classic  # Use the classic version explicitly
-      
-      # Elixir language server
+      nixpkgs-fmt
       elixir-ls
     ];
 
-    languages = {
-      language = [
-        {
-          name = "nix";
-          auto-format = true;
-          formatter = { command = "nixfmt-classic"; };
-          language-servers = [ "nil" "nixd" ];
-        }
-        {
-          name = "elixir";
-          auto-format = true;
-          language-servers = [ "elixir-ls" ];
-        }
-      ];
+    settings = import ./helix-settings.nix; # <-- Simplified import
 
-      language-server = {
-        nil = {
-          command = "nil";
-          config.nil = {
-            formatting.command = [ "nixfmt-classic" ];
-          };
-        };
-        
-        nixd = {
-          command = "nixd";
-        };
-        
-        elixir-ls = {
-          command = "elixir-ls";
-        };
-      };
-    };
-
-    settings = {
-      theme = "gruvbox";
-      editor = {
-        cursor-shape = {
-          insert = "bar";
-          normal = "block";
-          select = "underline";
-        };
-        lsp = {
-          display-messages = true;
-          display-inlay-hints = true;
-        };
-      };
-    };
+    languages = import ./helix-languages.nix { inherit pkgs; };
   };
-}
 
+  home.packages = with pkgs; [
+    fish
+    fzf
+    grc
+    lsd
+    helix
+    # Language servers & formatters (also ensures they are in PATH)
+    nil
+    nixd
+    nixpkgs-fmt
+    elixir-ls
+    elixir # Needed for `mix format` if you enable it
+  ];
+
+  programs.home-manager.enable = true;
+}
