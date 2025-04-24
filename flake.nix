f0e9d95b5f3d507246742202a00e18b494c2846e
Anwer Khan <deepwatrcreatur@gmail.com>
2025-04-24 02:12:31 -0400

    reorganizing nix configuration

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
+# flake.nix (Simplified)
 {
   description = "My nix-darwin configuration with Home Manager";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
@@ -10,26 +10,35 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
+  # Use the standard signature, only listing defined inputs and self
   outputs = { self, nix-darwin, nixpkgs, home-manager, ... }:
   let
     system = "aarch64-darwin"; # For Apple Silicon (M4)
-    pkgs = nixpkgs.legacyPackages.${system};
   in
   {
     darwinConfigurations.macminim4 = nix-darwin.lib.darwinSystem {
       inherit system;
+      # Remove specialArgs unless a module explicitly needs the 'inputs' set
+      # specialArgs = { inherit inputs; }; # REMOVED THIS LINE
       modules = [
+        # Your existing system configuration
         ./darwin-configuration.nix
-      ];
-    };
 
-    homeConfigurations.deepwatrcreatur = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      modules = [
-        ./home.nix
+        # --- Add Home Manager Integration ---
+        home-manager.darwinModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.deepwatrcreatur = import ./home.nix;
+            # Remove extraSpecialArgs unless home.nix explicitly needs 'inputs'
+            # extraSpecialArgs = { inherit inputs; }; # REMOVED THIS LINE
+          };
+        }
+        # --- End Home Manager Integration ---
       ];
     };
+    # No separate homeConfigurations block needed
   };
 }
 
