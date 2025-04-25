023b2969a10527d57b8840049fcd3578a1392193
Anwer Khan <deepwatrcreatur@gmail.com>
2025-04-25 16:00:36 -0400

    added homeConfigurations block

--- flake.nix
+++ flake.nix
@@ -1,44 +1,28 @@
-# flake.nix (Simplified)
 {
   description = "My nix-darwin configuration with Home Manager";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  # Use the standard signature, only listing defined inputs and self
   outputs = { self, nix-darwin, nixpkgs, home-manager, ... }:
-  let
-    system = "aarch64-darwin"; # For Apple Silicon (M4)
-  in
-  {
-    darwinConfigurations.macminim4 = nix-darwin.lib.darwinSystem {
-      inherit system;
-      # Remove specialArgs unless a module explicitly needs the 'inputs' set
-      # specialArgs = { inherit inputs; }; # REMOVED THIS LINE
-      modules = [
-        # Your existing system configuration
-        ./darwin-configuration.nix
-
-        # --- Add Home Manager Integration ---
-        home-manager.darwinModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.deepwatrcreatur = import ./home.nix;
-            # Remove extraSpecialArgs unless home.nix explicitly needs 'inputs'
-            # extraSpecialArgs = { inherit inputs; }; # REMOVED THIS LINE
-          };
-        }
-        # --- End Home Manager Integration ---
-      ];
+    let
+      system = "aarch64-darwin";
+    in {
+      darwinConfigurations.macminim4 = nix-darwin.lib.darwinSystem {
+        inherit system;
+        modules = [
+          ./darwin-configuration.nix
+        ];
+      };
+      homeConfigurations."deepwatrcreatur" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+        modules = [ ./home.nix ];
+      };
     };
-    # No separate homeConfigurations block needed
-  };
 }
 
